21f61abdbe648db054ee1c1b9f4bc53a7cdad4ed
Md Moniruzzaman Sojol <bsmrstueee90@gmail.com>
2021-06-04 01:54:29 +0600

    home screen done

--- App.js
+++ App.js
@@ -1,22 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { Onboarding, Destination } from "./Screens/index";
+import { icons, COLORS, SIZES } from './constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Here we go with trip plan</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize:18
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
   },
-});
+};
+
+const Stack = createStackNavigator();
+// const App = () => {
+//   return (
+
+//   );
+// };
+// export default () => {
+//   return <App />;
+// };
+
+const App = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator initialRoute={"Onboarding"}>
+        {/* screens */}
+        <Stack.Screen
+          name="Onboarding"
+          component={Onboarding}
+          options={{
+            title: null,
+            headerStyle: {
+              backgroundColor: COLORS.white,
+            },
+            headerLeft: null,
+            headerRight: () => {
+              <TouchableOpacity
+                style={{ marginRight: SIZES.padding }}
+                onPress={() => console.log("Pressed")}
+              >
+                <Image
+                  source={icons.barMenu}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              </TouchableOpacity>;
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ Screens/Destination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Destination = () => {
+    return (
+        <View>
+            <Text>Destination</Text>
+        </View>
+    );
+};
+
+export default Destination;
